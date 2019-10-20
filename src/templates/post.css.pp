9555107379db9322d3b410d3c5f1418b7f97e647
Ethan Riley <riley787cmplex@gmail.com>
2019-10-20 19:21:08 +0100

    fin css for now

--- src/templates/post.css.pp
+++ src/templates/post.css.pp
@@ -1,19 +1,44 @@
 #lang pollen
 
+.body{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
 .navi{
+    display: flex;
+    flex-grow: 1;
+    flex-wrap: wrap;
+    justify-content: space-around;
 }
-.traingle-up{
+.triangle-up {
+	width: 0;
+	height: 0;
+	border-left: 1em solid transparent;
+	border-right: 1em solid transparent;
+	border-bottom: 2em solid #000000;
 }
-.traingle-down{
+.triangle-down {
+	width: 0;
+	height: 0;
+	border-left: 1em solid transparent;
+	border-right: 1em solid transparent;
+	border-top: 2em solid #000000;
 }
 
 .snippet{
+    flex-grow: 2;
+    flex-align: flex-end;
 }
 
 .takeover{
+    flex-grow: 9999;
 }
 
 .text{
+    flex-grow: 2;
+    flex-align: flex-start;
 }
 
 
